77e2093a0d2ed37d785040767fa46a4f9dc2f31f
bvchand <65008811+bvchand@users.noreply.github.com>
2022-01-05 00:39:50 -0800

    Time: 1788 ms (16.04%), Space: 0B (100.00%) - LeetHub

--- 1454-active-users/1454-active-users.sql
+++ 1454-active-users/1454-active-users.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 SELECT DISTINCT a.id, c.name
-FROM (SELECT DISTINCT id, login_date,
+FROM   (SELECT DISTINCT id, login_date,
         DENSE_RANK() OVER(PARTITION BY id ORDER BY login_date) as rnk
         FROM Logins) a
 JOIN Accounts c ON a.id = c.id 
