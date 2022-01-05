9ebcc4cfd3453f00bd2a33748844c6360dbcccef
bvchand <65008811+bvchand@users.noreply.github.com>
2022-01-05 00:39:11 -0800

    Time: 1666 ms (23.13%), Space: 0B (100.00%) - LeetHub

---
+++ 1454-active-users/1454-active-users.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+SELECT DISTINCT a.id, c.name
+FROM (SELECT DISTINCT id, login_date,
+        DENSE_RANK() OVER(PARTITION BY id ORDER BY login_date) as rnk
+        FROM Logins) a
+JOIN Accounts c ON a.id = c.id 
+GROUP BY a.id, DATE_ADD(a.login_date, INTERVAL -rnk DAY)
+HAVING COUNT(a.id) >= 5
+
