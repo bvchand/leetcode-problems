01a434ca3d82217271989ff06501d2697bfdc2a8
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-09 15:08:12 -0800

    Time: 38 ms (55.31%), Space: 13.9 MB (81.90%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -5,22 +5,41 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # my implementation
+#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+#         self.sum_ = 0
+#         self.temp = ''
+                
+#         def helper(root):
+#             self.temp += str(root.val)
+#             if not root.left and not root.right:
+#                 self.sum_ += int(self.temp)
+#                 return
+#             if root.left:
+#                 helper(root.left)
+#                 self.temp = self.temp[:-1]
+#             if root.right:
+#                 helper(root.right)
+#                 self.temp = self.temp[:-1]
+
+#         helper(root)
+#         return self.sum_
+    
+    # preorder
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         self.sum_ = 0
-        self.temp = ''
                 
-        def helper(root):
-            self.temp += str(root.val)
+        def preorder(root, temp):
+            if not root: return
+            
+            temp = temp * 10 + root.val
             if not root.left and not root.right:
-                self.sum_ += int(self.temp)
+                self.sum_ += temp
                 return
-            if root.left:
-                helper(root.left)
-                self.temp = self.temp[:-1]
-            if root.right:
-                helper(root.right)
-                self.temp = self.temp[:-1]
+            else:
+                preorder(root.left, temp)
+                preorder(root.right, temp)
 
-        helper(root)
+        preorder(root, 0)
         return self.sum_
         
