66c1744290b4cbd9ecdab94d06e4ced1c843f92f
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-09 15:10:19 -0800

    Time: 55 ms (20.47%), Space: 13.8 MB (99.76%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -6,40 +6,36 @@
 #         self.right = right
 class Solution:
     # my implementation
-#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-#         self.sum_ = 0
-#         self.temp = ''
-                
-#         def helper(root):
-#             self.temp += str(root.val)
-#             if not root.left and not root.right:
-#                 self.sum_ += int(self.temp)
-#                 return
-#             if root.left:
-#                 helper(root.left)
-#                 self.temp = self.temp[:-1]
-#             if root.right:
-#                 helper(root.right)
-#                 self.temp = self.temp[:-1]
-
-#         helper(root)
-#         return self.sum_
-    
-    # preorder
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         self.sum_ = 0
                 
-        def preorder(root, temp):
+        def helper(root, temp):
             if not root: return
-            
-            temp = temp * 10 + root.val
+            temp += str(root.val)
             if not root.left and not root.right:
-                self.sum_ += temp
+                self.sum_ += int(temp)
                 return
-            else:
-                preorder(root.left, temp)
-                preorder(root.right, temp)
+            helper(root.left, temp)
+            helper(root.right, temp)
 
-        preorder(root, 0)
+        helper(root, '')
         return self.sum_
+    
+    # preorder
+#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+#         self.sum_ = 0
+                
+#         def preorder(root, temp):
+#             if not root: return
+            
+#             temp = temp * 10 + root.val
+#             if not root.left and not root.right:
+#                 self.sum_ += temp
+#                 return
+#             else:
+#                 preorder(root.left, temp)
+#                 preorder(root.right, temp)
+
+#         preorder(root, 0)
+#         return self.sum_
         
