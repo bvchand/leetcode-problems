6971efb25de7e3a6d557b91016b00531f93b72b2
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-09 19:29:30 -0800

    Time: 1219 ms (26.76%), Space: 20.5 MB (18.41%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -3,7 +3,13 @@ class Solution:
         def square_distance(point):
             x,y = point
             return math.sqrt(x**2+y**2)
-        points.sort(key=square_distance)
-        return points[:k]
+        heap = [(-square_distance(points[i]), points[i]) for i in range(k)]      # [(dist,point)]
+        heapq.heapify(heap)
         
+        for i in range(k, len(points)):
+            new_dist = -square_distance(points[i])
+            smalleest_dist_so_far = heap[0][0]
+            if new_dist > smalleest_dist_so_far:
+                heapq.heappushpop(heap, (new_dist, points[i]))
+        return [i for (_,i) in heap]
         
