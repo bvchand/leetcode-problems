c51152504412e2f822a24960cfa62162c9b6e458
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-16 10:20:54 -0800

    Time: 541 ms (17.92%), Space: 15.2 MB (81.82%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,24 +1,16 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        pLetters, sLetters = {}, {}
+        pLetters, sLetters = Counter(p), Counter()
+        pLen, sLen = len(p), len(s)
         result = []
         
-        for c in p:
-            pLetters[c] = 1 if c not in pLetters else pLetters[c] + 1
-        
-        start, end, i = 0, len(p), 0
-        while end <= len(s):
-            if i >= start and i < end:
-                char = s[i]
-                sLetters[char] = 1 if char not in sLetters else sLetters[char] + 1
-                i += 1
-            elif i == end:
-                if sLetters == pLetters:
-                    result.append(start)
-                if sLetters[s[start]] - 1 == 0:  sLetters.pop(s[start])
-                else:   sLetters[s[start]] -= 1
-                start += 1
-                end += 1
+        for i in range(sLen):
+            sLetters[s[i]] += 1
+            if i >= pLen:
+                if sLetters[s[i-pLen]] == 1:  del sLetters[s[i-pLen]]
+                else:   sLetters[s[i-pLen]] -= 1
+            if sLetters == pLetters:
+                    result.append(i-pLen+1)
 
         return result
                     
