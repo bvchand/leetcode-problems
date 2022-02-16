5c1c05589135e42ab8019ccf12671551fbc9c207
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-16 15:15:37 -0800

    Time: 759 ms (20.00%), Space: 18.3 MB (12.98%) - LeetHub

---
+++ 934-shortest-bridge/934-shortest-bridge.py
@@ -0,0 +1,59 @@
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        flag = False
+        
+        # flood-fill - DFS
+        def flood_fill(r, c):
+            if  r<0 or r>=ROWS or c<0 or c>=COLS or grid[r][c] != 1: 
+                return
+            
+            grid[r][c] = 2
+            visited.add((r,c))
+            
+            flood_fill(r+1, c)
+            flood_fill(r-1, c)
+            flood_fill(r, c+1)
+            flood_fill(r, c-1)
+            
+            return
+        
+        for i, j in product(range(ROWS), range(COLS)):
+            if grid[i][j] == 1:
+                flood_fill(i, j) # paint one island to 2, border 0 add to q
+                break
+        
+        step = -1
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        queue = deque(visited)
+        
+        # BFS - find the minimum distance
+        while queue:
+            print(queue, step)
+            for _ in range(len(queue)):
+                r,c = queue.popleft()
+                print("r, c: ", r, c)
+                for dx,dy in directions:
+                    x, y = r+dx, c+dy
+                    print("dx, dy: ", x, y)
+                    if x>=0 and x<ROWS and y>=0 and y<COLS:
+                        if grid[x][y] == 1:     return step+1
+                        elif grid[x][y] == 0 and (x,y) not in visited:   
+                            queue.append((x,y))
+                            visited.add((x,y))
+            step += 1
+                        
+        return step
+            
+            
+        
+        
+        
+        
+        
+        
+            
+                
+                
+        
