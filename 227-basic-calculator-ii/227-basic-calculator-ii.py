aa1fd2d7c05a4faccbf7f78ecbbdaf232f694b02
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-20 12:50:12 -0800

    Time: 120 ms (46.51%), Space: 15.6 MB (69.01%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        value = []
+        curr_num = 0
+        operator = '+'
+        s = ''.join(s.split())
+        
+        for i in range(len(s)):
+            char = s[i]
+            if char in string.digits:
+                curr_num = curr_num * 10 + int(char)
+            if char in '+-*/' or i == len(s)-1: 
+                if operator == '+':
+                    value.append(curr_num)
+                if operator == '-':
+                    value.append(-curr_num)
+                elif operator == '*':
+                    value[-1] *= curr_num
+                elif operator == '/':
+                    value[-1] = int(value[-1]/curr_num)
+                curr_num = 0
+                operator = char
+
+        return sum(value)
+            
