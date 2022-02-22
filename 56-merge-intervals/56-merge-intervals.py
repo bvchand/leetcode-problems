5da6a594e16afb0785f6647d62a0bb8dbabc98fc
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-21 20:35:08 -0800

    Time: 237 ms (28.89%), Space: 18.1 MB (69.02%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,5 +1,6 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
         intervals.sort()    # sort the intervals based on the start time
         print(intervals)
         result = [intervals[0]]
@@ -11,6 +12,19 @@ class Solution:
                 result.append(intervals[i])
                 
         return result
-            
+        """
+        
+        intervals.sort() 
+        result = [intervals[0], ]
+        
+        for i in range(1, len(intervals)):
+            if result[-1][1] >= intervals[i][0]:
+                result[-1][1] = max(result[-1][1], intervals[i][1])
+            else:
+                result.append(intervals[i])
+        
+        return result
+        
+        
         
         
