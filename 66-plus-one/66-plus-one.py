b939ef774237c8344cc3c415c177842981b3787e
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-24 11:05:34 -0800

    Time: 52 ms (35.08%), Space: 13.8 MB (84.26%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -6,14 +6,13 @@
 
 
 time: O(n)
-space = O(n)
+space = O(1)
 """
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         i = len(digits)-1
         carry = 1
-        # result = []
 
         while i >= 0:
             res = digits[i] + carry
@@ -21,7 +20,6 @@ class Solution:
             carry = res // 10 if res > 9 else 0
             i -= 1
             
-        # if carry:   result.append(carry)
         return [carry] + digits if carry else digits
 
     
