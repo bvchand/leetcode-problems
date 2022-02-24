208db34786bb6faa5d4a35468309991645a165e3
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-24 11:05:24 -0800

    Time: 65 ms (9.81%), Space: 14 MB (61.61%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,22 +1,30 @@
 """
 [1,2,3]
+[9]
+[1,2,-3]
+[]
 
+
+time: O(n)
+space = O(n)
 """
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         i = len(digits)-1
         carry = 1
-        result = []
+        # result = []
 
         while i >= 0:
             res = digits[i] + carry
-            result.append(res % 10)
+            digits[i] = res % 10
             carry = res // 10 if res > 9 else 0
             i -= 1
             
-        if carry:   result.append(carry)
-        return result[::-1]
+        # if carry:   result.append(carry)
+        return [carry] + digits if carry else digits
+
+    
                 
                 
                 
