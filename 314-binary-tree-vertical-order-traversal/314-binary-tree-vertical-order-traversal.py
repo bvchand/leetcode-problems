c0807192bf4852cdf57d71cfed8d5ab42e22a135
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-24 23:45:57 -0800

    Time: 41 ms (65.61%), Space: 13.9 MB (71.29%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -4,21 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BFS 
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         col_dict = defaultdict(list)
         visited = deque()
         visited.append((root,0))
+        min_col, max_col = sys.maxsize, 0
         
         while visited:
-            # print(visited)
             node, col = visited.popleft()
             
             if node:
                 col_dict[col].append(node.val)
+                if col < min_col:   min_col = col
+                elif col > max_col: max_col = col
                 visited.append((node.left, col-1))
                 visited.append((node.right, col+1))
         
-        return [col_dict[key] for key in sorted(col_dict.keys())]
+        return [col_dict[i] for i in range(min_col, max_col+1)]
             
         
