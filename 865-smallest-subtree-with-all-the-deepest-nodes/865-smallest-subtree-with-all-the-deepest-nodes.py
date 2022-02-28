7d8e53d22c7154223d5c3d85c56e5f6639d24678
bvchand <65008811+bvchand@users.noreply.github.com>
2022-02-27 22:03:20 -0800

    Time: 51 ms (49.20%), Space: 14.1 MB (77.53%) - LeetHub

---
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        depth = {None:-1,}
+        
+        def dfs(root, parent=None):
+            if root:
+                depth[root] = depth[parent] + 1
+                dfs(root.left, root)
+                dfs(root.right, root)
+                
+        def get_deepest_subtree(root):
+            if not root or depth[root] == max_depth:
+                return root
+            L = get_deepest_subtree(root.left)
+            R = get_deepest_subtree(root.right)
+            
+            return root if (L and R) else (L or R)
+        
+        dfs(root)
+        max_depth = max(depth.values())
+    
+        return get_deepest_subtree(root)
