6b5ccf400f45ce1e874dd9d75455e687c12d81e9
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-10 13:45:17 -0800

    Time: 72 ms (89.78%), Space: 26.1 MB (70.31%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -4,31 +4,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-"""
-root = [            3,
-           5,              1,
-        6,       2,      0,    8,
-               7, 4 
-                
-    p = 5, q = 4
-
-root is none -> (num of nodes found = 0, ancestor = root)
-l = helper(root.left)
-r = helper(root.right)
-if root in nodes:
-    num_nodes = 1+l[0]+r[0]
-else:
-    num_nodes = l[0]+r[0]
-return (num_nodes, root)
-
-
-time=O(N)
-space=O(N)
-
-
-"""
-
-
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -36,25 +11,22 @@ class Solution:
         
         def helper(root):
             if not root:
-                return (0, None)        # (num_nodes, ancestor)
+                return (0, None)    # (number of nodes, ancestor)
             
             left_result = helper(root.left)
             if left_result[0] == 2:     return left_result
             
+            
             right_result = helper(root.right)
             if right_result[0] == 2:     return right_result
             
-            if root not in nodes:
-                num_nodes = left_result[0] + right_result[0]
-            else:
+            
+            if root in nodes:
                 num_nodes = 1 + left_result[0] + right_result[0]
+            else:
+                num_nodes = left_result[0] + right_result[0]
             
-            print(root.val, num_nodes)
-            return (num_nodes, root if num_nodes==2 else None)
+            
+            return (num_nodes, root) if num_nodes == 2 else (num_nodes, None)
         
-        res = helper(root)
-        # print(res)
-        return res[1]
-                
-            
-            
+        return helper(root)[1]
