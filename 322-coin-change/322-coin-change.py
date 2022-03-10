68d89e2e7684b8ca4865d7bf61a1214f12780733
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-10 10:24:44 -0800

    Time: 1945 ms (53.87%), Space: 14 MB (89.06%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,6 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount+1] * (amount+1)    # cache
+        dp = [sys.maxsize] * (amount+1)    # cache
         dp[0] = 0                        # zero coins needed to accumulate zero amount
         
         for a in range(1, amount+1):
@@ -8,7 +8,7 @@ class Solution:
                 if c <= a:
                     dp[a] = min(1+dp[a-c], dp[a])
         
-        return dp[amount] if dp[amount] != amount+1 else -1
+        return dp[amount] if dp[amount] != sys.maxsize else -1
             
         
         
