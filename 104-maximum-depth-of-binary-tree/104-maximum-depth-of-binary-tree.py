7f1d4d8505d8db67ce8d10c7865cf7c8ed0e9d95
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-10 12:06:36 -0800

    Time: 60 ms (48.58%), Space: 16.3 MB (24.16%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -15,3 +15,10 @@ class Solution:
             return max(1 + helper(root.left, depth), 1 + helper(root.right, depth))
         
         return helper(root, 1)
+    
+
+"""
+Time:  O(N)
+Space: O(N) if tree is unbalanced -- each node is in one level -- recursion call will occur N times -- worst case
+       O(log(N)) if tree is perfectly balanced -- best case
+"""
