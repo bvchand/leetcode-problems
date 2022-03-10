3b24e2a5a2fc9ea73caf3d7175f34172b7c4a95a
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-10 08:46:05 -0800

    Time: 28 ms (93.71%), Space: 13.8 MB (75.33%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,7 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         first, second = 1, 1        # first -- at last step; second -- from penultimate step
-        res = 0
         
         for i in range(n-1):
             temp = second
