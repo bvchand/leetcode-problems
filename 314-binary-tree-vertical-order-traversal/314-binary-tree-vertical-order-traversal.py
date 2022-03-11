913a0d5cf80f0d48b7a99d8962a3ddd56c1f4e1b
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-11 10:27:03 -0800

    Time: 56 ms (34.52%), Space: 14 MB (56.26%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -5,23 +5,32 @@
 #         self.left = left
 #         self.right = right
 
-# BFS 
+from collections import defaultdict, deque
+
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        col_dict = defaultdict(list)
-        visited = deque([(root,0)])
+        colMap = defaultdict(list)
+        visited = deque()
+        visited.append((root, 0))       # (node, column)
         min_col, max_col = sys.maxsize, 0
         
         while visited:
             node, col = visited.popleft()
-            
             if node:
-                col_dict[col].append(node.val)
+                colMap[col].append(node.val)
                 if col < min_col:   min_col = col
                 elif col > max_col: max_col = col
-                visited.append((node.left, col-1))
-                visited.append((node.right, col+1))
+                visited.append((node.left, col-1)) 
+                visited.append((node.right, col+1)) 
         
-        return [col_dict[i] for i in range(min_col, max_col+1)]
+        return [colMap[i] for i in range(min_col, max_col+1)]
+    
+"""
+BFS
+time: O(N)
+space: O(N)
+"""
             
+                
+                
         
