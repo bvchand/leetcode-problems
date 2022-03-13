4a243d71e3fd37f2a1fa81b56bd2ecd63d87f2a3
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-13 13:50:08 -0700

    Time: 112 ms (83.97%), Space: 16.3 MB (22.33%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,41 +1,25 @@
-"""
-0 1 2 3 4 5 6 7 8 9 10 11 12
-l e e ( t ( c ) o ) d  e  )
-
-
-
-stack = []
-set = (12)
-res = 
-
-"""
-
-
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
         invalid_index = set()
+        stack = []
+        
+        for i,c in enumerate(s):
+            if c not in '()':
+                continue
+            elif c == '(':
+                stack.append(i)
+            elif stack and c == ')':
+                stack.pop()
+            elif c == ')':
+                invalid_index.add(i)
+        
+        if stack:  
+            for i in stack:     invalid_index.add(i)
+        
         res = []
         
         for i in range(len(s)):
-            if s[i] not in '()':
-                continue
-            elif s[i] == '(':
-                stack.append(i)
-            elif stack and s[i] == ')':
-                stack.pop()
-            elif s[i] == ')':
-                invalid_index.add(i)
-        
-        if stack:
-            for i in stack:      invalid_index.add(i) 
-        
-        for i in range(len(s)):
-            if i not in invalid_index:
-                res.append(s[i])
+            if i not in invalid_index:  res.append(s[i])
         
         return ''.join(res)
-        
-                
-        
         
