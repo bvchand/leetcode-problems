aa78cab216a0370662db0f3fb258882773fc0215
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-15 17:00:59 -0700

    Time: 40 ms (77.56%), Space: 13.9 MB (48.79%) - LeetHub

---
+++ 65-valid-number/65-valid-number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        seen_digit, seen_exp, seen_dot = 0, 0, 0
+        signs = ('+', '-')
+        
+    
+        for i in range(len(s)):
+            if s[i].isdigit():
+                seen_digit = True
+            
+            elif s[i] in signs:
+                if i > 0 and s[i-1] not in ('e', 'E'):
+                    return False
+            
+            elif i > 0 and s[i] in ('e', 'E'):
+                if seen_exp or not seen_digit:
+                    return False
+                seen_exp = True
+                seen_digit = False
+            
+            elif s[i] == '.':
+                if seen_dot or seen_exp:
+                    return False
+                seen_dot = True
+            
+            else:
+                return False
+            
+        return seen_digit
