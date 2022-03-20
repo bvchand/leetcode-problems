76b7cdb56cb351376a2a78fc5667c5b623325008
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-20 13:03:12 -0700

    Time: 64 ms (10.11%), Space: 14 MB (93.83%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.py
@@ -0,0 +1,65 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        self.sum = 0
+        depth = 1
+        
+        def dfs(nlist, depth):
+            for element in nlist:
+                if element.isInteger():
+                    self.sum += depth * element.getInteger()
+                    continue
+                dfs(element.getList(), depth+1)
+    
+        dfs(nestedList, depth)
+            
+        return self.sum
+
+"""
+DFS
+time: O(N)
+space: O(D); D->depth; worse case D=N
+"""
+        
