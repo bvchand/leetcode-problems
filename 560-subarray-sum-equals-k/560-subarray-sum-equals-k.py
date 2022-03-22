f349c52ed65dbe0f459b6e06140fd1225685f5bd
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-21 18:14:15 -0700

    Time: 338 ms (55.80%), Space: 16.5 MB (98.50%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,16 +1,6 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-#         hash = defaultdict(int)
-#         hash[0] = 1
-#         curr_sum, count = 0, 0
-        
-#         for i in range(len(nums)):
-#             curr_sum += nums[i]
-#             if curr_sum - k in hash:
-#                     count += hash[curr_sum - k]
-#             hash[curr_sum] += 1
-        
-#         return count
+
         hash = defaultdict(int)
         hash[0] = 1
         curr_sum = count = 0
@@ -22,3 +12,8 @@ class Solution:
             hash[curr_sum] += 1
         
         return count
+
+    """
+    time: O(N)
+    space: O(N)
+    """
