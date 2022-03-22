127ca4896f88bebf801b6688b4c997f49fddb6dc
bvchand <65008811+bvchand@users.noreply.github.com>
2022-03-22 09:28:12 -0700

    Time: 1157 ms (55.43%), Space: 18.2 MB (41.65%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,31 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        end = n-1
         nums.sort()
         result = []
         
-        for i in range(n):
-            if nums[i] > 0:     break
-            if i==0 or nums[i-1] != nums[i]:
-                self.twoSum(nums, i, result)
+        for i, a in enumerate(nums):
+            if i > 0 and nums[i-1] == a:
+                continue
             
+            l, r = i+1, len(nums)-1
+            while l < r:
+                threesum = a + nums[l] + nums[r]
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                else:
+                    result.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
         return result
-    
-    def twoSum(self, nums: List[int], i: int, result:List[List[int]]):
-        target = nums[i]
-        seen = set()
-        j =i+1
-        
-        while j < len(nums):
-            diff = -target - nums[j]
-            if diff in seen:
-                result.append([target, nums[j], diff])
-                while j + 1 < len(nums) and nums[j] == nums[j + 1]:
-                    j += 1
+
+    """
+    time: O(N^2)
+    space: O(1)
+    """
             
-            seen.add(nums[j])
-            j += 1
+        
+        
         
         
             
