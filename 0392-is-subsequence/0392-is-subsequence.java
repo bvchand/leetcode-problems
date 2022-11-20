005e7cacc009108d61628ef3d7d94d0b257cea0f
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-19 21:46:42 -0800

    Time: 5 ms (28.46%), Space: 42 MB (45.93%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int p1 = 0 , p2 = 0;
+        int charFound = 0;
+        if (s.length() == 0)
+            return true;
+        if (s.length() > t.length())
+            return false;
+        
+        while (p1 < s.length() && 
+               p2 < t.length()) {
+            char c1 = s.charAt(p1);
+            char c2 = t.charAt(p2);
+            if (c1 == c2) {
+                charFound++;
+                p1++;
+                p2++;
+            } else {
+                p2++;
+            }
+        }
+        if (charFound == s.length()) {
+            return true;
+        }
+        return false;
+    }
+}
