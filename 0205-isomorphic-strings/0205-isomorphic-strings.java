fc0ff318c11ffe7334a3aa362fb92ab73aa40651
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-19 21:07:13 -0800

    Time: 3 ms (99.54%), Space: 42 MB (95.76%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         int[] s2t = new int[256], t2s = new int[256];
-        
         Arrays.fill(s2t, -1);
         Arrays.fill(t2s, -1);
-        
-        if (s.length() != t.length())
-            return false;
         
         for (int i=0; i<s.length(); i++) {
             char c1 = s.charAt(i), c2 = t.charAt(i);
@@ -15,12 +11,12 @@ class Solution {
                 s2t[c1] = c2;
                 t2s[c2] = c1;
             }
-                
-            if (!(s2t[c1] == c2 && t2s[c2] == c1)){
-                return false;
-            }
             
+            if (!(s2t[c1] == c2 && t2s[c2] == c1)) {
+                return false;
+            }  
         }
+        
         return true;
     }
 }
