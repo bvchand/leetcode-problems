57f5b0856b7cab55745cf614e6cfeb799d94f4c9
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-22 15:19:47 -0800

    Time: 18 ms (53.70%), Space: 53.7 MB (71.81%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,3 +1,15 @@
+/*
+Strategy:
+- bring all the values under the range of 60 by n%60.
+- subtract the new values by 60 to get its complement and see if we have already encountered that. If we have increment the result counter.
+- complement%60 is done for all the numbers divisible by 60 as they result in 0.
+- increment the seen count for the current remainder.
+
+time: O(N)
+space: O(60) = O(1)
+
+*/
+
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         int res = 0;
@@ -10,6 +22,5 @@ class Solution {
             seen[rem]++;
         }
         return res;
-        
     }
 }
