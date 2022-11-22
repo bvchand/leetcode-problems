eb1bc3d23769832d77250450b6392d8ef73d8383
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-21 23:19:09 -0800

    Time: 3 ms (82.03%), Space: 54.6 MB (19.28%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
@@ -1,18 +1,56 @@
 /*
-Strategy: Sorting
-time: O(nlogn)
+Strategy: Math (decrementing one element by one is equilvalent to incrementing n-1 elements by one)
+- Try to find the distance between each element and the min element in the array
+
+time: O(n)
 space: O(1)
 */
+
 class Solution {
     public int minMoves(int[] nums) {
         int moves = 0;
-        
-        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
         
         for (int i=0; i<nums.length; i++) {
-            moves += nums[i] - nums[0];
+            moves += nums[i] - min;
         }
         
         return moves;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*
+// Strategy: Sorting
+// time: O(nlogn)
+// space: O(1)
+// */
+// class Solution {
+//     public int minMoves(int[] nums) {
+//         int moves = 0;
+        
+//         Arrays.sort(nums);
+        
+//         for (int i=0; i<nums.length; i++) {
+//             moves += nums[i] - nums[0];
+//         }
+        
+//         return moves;
+//     }
+// }
