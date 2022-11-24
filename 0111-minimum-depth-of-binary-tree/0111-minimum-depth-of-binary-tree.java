9b33721d2d79682d2f5ec5ba74bfc4ed29c5dbab
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-23 16:16:49 -0800

    Time: 49 ms (5.59%), Space: 63.2 MB (81.83%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -13,23 +13,66 @@
  *     }
  * }
  */
+
+/*
+Approach = DFS
+*/
+
 class Solution {
     public int minDepth(TreeNode root) {
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        
         if (root == null)
             return 0;
-        
-        if (root.left == null && root.right == null)
-            return 1;
+
+        stack.add(new Pair(root, 1));
         
         int min_depth = Integer.MAX_VALUE;
-        if (root.left != null) {
-            min_depth = Math.min(min_depth, minDepth(root.left));
+        
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> curr = stack.pop();
+            
+            TreeNode currNode = curr.getKey();
+            int currDepth = curr.getValue();
+            
+            if (currNode.left == null && currNode.right == null)
+                min_depth = Math.min(min_depth, currDepth);
+            
+            if (currNode.right != null)
+                stack.push(new Pair(currNode.right, currDepth+1));
+            
+            if (currNode.left != null)
+                stack.push(new Pair(currNode.left, currDepth+1));
         }
         
-        if (root.right != null) {
-            min_depth = Math.min(min_depth, minDepth(root.right));
-        }
-        
-        return min_depth + 1;
+        return min_depth;
     }
 } 
+
+
+
+// /*
+// Approach: Recursive
+// space: O(1)
+// time: O(n)
+// */
+// class Solution {
+//     public int minDepth(TreeNode root) {
+//         if (root == null)
+//             return 0;
+        
+//         if (root.left == null && root.right == null)
+//             return 1;
+        
+//         int min_depth = Integer.MAX_VALUE;
+//         if (root.left != null) {
+//             min_depth = Math.min(min_depth, minDepth(root.left));
+//         }
+        
+//         if (root.right != null) {
+//             min_depth = Math.min(min_depth, minDepth(root.right));
+//         }
+        
+//         return min_depth + 1;
+//     }
+// } 
