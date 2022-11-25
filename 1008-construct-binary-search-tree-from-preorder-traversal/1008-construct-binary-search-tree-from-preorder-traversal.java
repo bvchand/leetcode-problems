48bc50c8d19e3cc5d51f248f98919fd82e75859f
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-25 00:15:38 -0800

    Time: 4 ms (9.91%), Space: 42.8 MB (6.31%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -13,30 +13,65 @@
  *     }
  * }
  */
-class Solution {
-    public int[] preorder;
-    public int index;
-    
-    public TreeNode helper(int lower, int upper) {
-        if (index == preorder.length)
-            return null;
+
+/*
+Iterative - using stack
+space: O(N)
+time: O(N)
+*/
+class Solution {    
+    public TreeNode bstFromPreorder(int[] preorder) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        stack.add(root);
         
-        int value = preorder[index];
-        
-        if (value < lower || value > upper)
-            return null;
-        
-        index++;
-        TreeNode root = new TreeNode(value);
-        root.left = helper(lower, value);
-        root.right = helper(value, upper);
-        
+        // traverse through the preorder array one-by-one
+        for (int i=1; i<preorder.length; i++) {
+            TreeNode parent = stack.peek();
+            TreeNode child = new TreeNode(preorder[i]);
+            
+            while (!stack.isEmpty() && child.val > stack.peek().val) {
+                parent = stack.pop();
+            }
+            
+            if (child.val < parent.val)     parent.left = child;
+            else parent.right = child;
+            stack.push(child);
+        }
         return root;
     }
-    
-    public TreeNode bstFromPreorder(int[] preorder) {
-        this.preorder = preorder;
-        index = 0;
-        return helper(Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
 }
+
+
+// /*
+// Recursion
+// space: O(N)
+// time: O(N)
+// */
+// class Solution {
+//     public int[] preorder;
+//     public int index;
+    
+//     public TreeNode helper(int lower, int upper) {
+//         if (index == preorder.length)
+//             return null;
+        
+//         int value = preorder[index];
+        
+//         if (value < lower || value > upper)
+//             return null;
+        
+//         index++;
+//         TreeNode root = new TreeNode(value);
+//         root.left = helper(lower, value);
+//         root.right = helper(value, upper);
+        
+//         return root;
+//     }
+    
+//     public TreeNode bstFromPreorder(int[] preorder) {
+//         this.preorder = preorder;
+//         index = 0;
+//         return helper(Integer.MIN_VALUE, Integer.MAX_VALUE);
+//     }
+// }
