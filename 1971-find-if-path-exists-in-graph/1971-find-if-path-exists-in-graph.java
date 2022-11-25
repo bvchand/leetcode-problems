61c2d2f4f7275e21267b5488f43f2c5b3a4eaa47
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-25 11:42:17 -0800

    Time: 264 ms (32.93%), Space: 200.7 MB (49.73%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -24,7 +24,6 @@ class Solution {
         
         while (!stack.isEmpty()) {
             int currNode = stack.pop();
-            
             if (currNode == destination)
                 return true;
             
