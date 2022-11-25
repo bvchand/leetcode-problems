4948008f97449762b0600f6af740b6f051915b53
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-25 11:41:42 -0800

    Time: 213 ms (47.80%), Space: 202.4 MB (47.87%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,16 +1,14 @@
 /*
-DFS - recursive
+DFS - iterative
 time = O(m+n) ; m: nodes/vertices, n:edges; O(m) for building the map; O(n) for finding the dest
 space = O(m+n) ; O(m) for map, O(n) recursive stack call for each node
 */
 class Solution {
-    private Map<Integer, List<Integer>> graph = new HashMap<>();
-    private int destination; 
     
     
-    public boolean validPath (int n, int[][] edges, int source, int destination) {
-        this.destination = destination;
-        
+    public boolean validPath (int n, int[][] edges, int source, int destination) {     
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
         boolean[] visited = new boolean[n];
         Arrays.fill(visited, false);
         
@@ -22,20 +20,63 @@ class Solution {
             graph.computeIfAbsent(y, val -> new ArrayList<Integer>()).add(x);
         }
         
-        return helper(source, visited);
-    }
-    
-    public boolean helper (int currNode, boolean[] visited) {
-        if (currNode == destination)
-            return true;
-
-        if (visited[currNode] == false) {
-            visited[currNode] = true;
-            for (int neighbor : graph.get(currNode)) {
-                if (helper(neighbor, visited))
-                    return true;
+        stack.add(source);
+        
+        while (!stack.isEmpty()) {
+            int currNode = stack.pop();
+            
+            if (currNode == destination)
+                return true;
+            
+            for (int neighbor: graph.get(currNode)) {
+                if (visited[neighbor] == false) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
             }
         }
         return false;
     }
 }
+
+// /*
+// DFS - recursive
+// time = O(m+n) ; m: nodes/vertices, n:edges; O(m) for building the map; O(n) for finding the dest
+// space = O(m+n) ; O(m) for map, O(n) recursive stack call for each node
+// */
+// class Solution {
+//     private Map<Integer, List<Integer>> graph = new HashMap<>();
+//     private int destination; 
+    
+    
+//     public boolean validPath (int n, int[][] edges, int source, int destination) {
+//         this.destination = destination;
+        
+//         boolean[] visited = new boolean[n];
+//         Arrays.fill(visited, false);
+        
+//         for (int[] edge : edges) {
+//             int x = edge[0], y = edge[1];
+            
+//             // map x -> [y] and y -> [x]
+//             graph.computeIfAbsent(x, val -> new ArrayList<Integer>()).add(y);
+//             graph.computeIfAbsent(y, val -> new ArrayList<Integer>()).add(x);
+//         }
+        
+//         return helper(source, visited);
+//     }
+    
+//     public boolean helper (int currNode, boolean[] visited) {
+//         if (currNode == destination)
+//             return true;
+
+//         if (visited[currNode] == false) {
+//             visited[currNode] = true;
+//             for (int neighbor : graph.get(currNode)) {
+//                 if (helper(neighbor, visited))
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
