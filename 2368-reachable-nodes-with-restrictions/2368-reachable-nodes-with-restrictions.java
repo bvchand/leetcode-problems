8e07b3cdd014f3b8c5ce0ddf3bb5e88fa69edaa8
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-28 11:18:50 -0800

    Time: 254 ms (31.79%), Space: 229.2 MB (31.80%) - LeetHub

---
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int reachableNodes(int n, int[][] edges, int[] restricted) {
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> restrictedSet = new HashSet<>();
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        
+        for (int[] edge: edges) {
+            int x = edge[0], y=edge[1];
+            adjList.computeIfAbsent(x, val -> new ArrayList<Integer>()).add(y);
+            adjList.computeIfAbsent(y, val -> new ArrayList<Integer>()).add(x);
+        }
+        
+        for (int node: restricted) {
+            restrictedSet.add(node);
+        }
+        
+        stack.push(0);
+        
+        while (!stack.isEmpty()) {
+            int currNode = stack.pop();
+            visited.add(currNode);
+            
+            for (int neighbor: adjList.get(currNode)) {
+                if (!visited.contains(neighbor)) {
+                    if (restrictedSet.contains(neighbor))
+                        continue;
+                    visited.add(neighbor);
+                    stack.push(neighbor);
+                }
+            }
+        }
+        
+        return visited.size();
+        
+    }
+}
