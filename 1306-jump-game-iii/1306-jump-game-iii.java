05faabbba9739d41c6907472a4f222ae3adc1a49
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-28 19:25:33 -0800

    Time: 29 ms (19.53%), Space: 61.9 MB (37.49%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean[] visited = new boolean[arr.length];
+        Queue<Integer> queue = new LinkedList<>();
+        
+        queue.add(start);
+        visited[start] = true;
+        
+        while(!queue.isEmpty()) {
+            int index = queue.poll();
+            if(arr[index] == 0)
+                return true;
+            for(int newIndex: new int[]{index + arr[index], index - arr[index]}) {
+                if(newIndex >= 0 && newIndex < arr.length && !visited[newIndex]) {
+                    queue.offer(newIndex);
+                    visited[newIndex] = true;
+                }
+            }
+        }
+        return false;
+    }
+}
