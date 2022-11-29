556879f6a13709361d047920acd670bbd5ad6f3e
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-29 12:23:37 -0800

    Time: 18 ms (6.25%), Space: 42.9 MB (5.96%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,12 +1,13 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        int i=0, j=0;
         int res = 0;
         String[] s1 = version1.split("\\.");
         String[] s2 = version2.split("\\.");
-        while (i <= s1.length || j <= s2.length) {
+        
+        int maxLength = Math.max(s1.length, s2.length);
+        for (int i=0; i<maxLength; i++) {
             int v1 = i < s1.length ? Integer.parseInt(s1[i]) : 0;
-            int v2 = j < s2.length ? Integer.parseInt(s2[j]) : 0;
+            int v2 = i < s2.length ? Integer.parseInt(s2[i]) : 0;
             
             System.out.println(v1 + " " + v2);
             
@@ -15,10 +16,6 @@ class Solution {
             }
             else if (v1 > v2) {
                 return 1;
-            }
-            else {
-                i++;
-                j++;
             }
         }
         return res;
