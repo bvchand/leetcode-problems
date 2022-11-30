e5a139fa0c4d09563f983ed6497703b24f80c25f
bvchand <65008811+bvchand@users.noreply.github.com>
2022-11-30 14:14:16 -0800

    Time: 33 ms (13.21%), Space: 52.5 MB (7.86%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,88 @@
+class Solution {
+    
+    public boolean detectCycleHelper(int course, Map<Integer, List<Integer>> adjList, int[] visited) {
+        
+        // if a course is visited
+        if(visited[course] == 1)
+            return true;
+        // if a course is visited and processed (all child nodes are visited)
+        if(visited[course] == 2)
+            return false;
+        
+        visited[course] = 1;
+        List<Integer> prereq = adjList.getOrDefault(course, new ArrayList<Integer>());
+        // System.out.println(prereq.size());
+        for(int i=0; i<prereq.size(); i++) {
+            if(detectCycleHelper(prereq.get(i), adjList, visited))
+                return true;
+        }
+        visited[course] = 2;
+        return false;
+    }
+    
+    // technique for detecting cycle: graph coloring technique (dfs)
+    public boolean detectCycle(Map<Integer, List<Integer>> adjList, int numCourses) {
+        int[] visited = new int[numCourses];
+        Arrays.fill(visited, 0);
+        
+        for(int i=0; i<numCourses; i++) {
+            if(visited[i] == 0)
+                if(detectCycleHelper(i, adjList, visited))
+                    return true;
+        }
+        return false;
+    }
+    
+    
+    public void dfs(int course, int numCourses, Map<Integer, List<Integer>> adjList, boolean[] visited, Stack<Integer> stack) {
+        visited[course] = true;
+        List<Integer> prereq = adjList.getOrDefault(course, new ArrayList<Integer>());
+        // System.out.println(prereq.toString());
+        for(int i=0; i<prereq.size(); i++) {
+            if(!visited[prereq.get(i)]) {
+                visited[prereq.get(i)] = true;
+                dfs(prereq.get(i), numCourses, adjList, visited, stack);
+            }
+        }
+        stack.add(course);
+    }
+    
+        
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        
+        for(int[] courses: prerequisites) {
+            // List<Integer> lst = adjList.getOrDefault(courses[0], new ArrayList<Integer>());
+            // lst.add(courses[1]);
+            // adjList.put(courses[0], lst);
+            adjList.computeIfAbsent(courses[1], val -> new ArrayList<Integer>()).add(courses[0]);
+        }
+        
+        System.out.println(adjList.toString());
+        
+        int[] result = {};
+        
+        if(detectCycle(adjList, numCourses)) {
+            System.out.println("here");
+            return result;
+        }
+            
+        
+        boolean[] visited = new boolean[numCourses];
+        Arrays.fill(visited, false);
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0; i<numCourses; i++) {
+            if(!visited[i]) {
+                dfs(i, numCourses, adjList, visited, stack);
+            }
+        }
+        
+        result = new int[numCourses];
+        for(int i=0; i<numCourses; i++)
+            result[i] = stack.pop();
+        
+        return result;
+    }
+}
