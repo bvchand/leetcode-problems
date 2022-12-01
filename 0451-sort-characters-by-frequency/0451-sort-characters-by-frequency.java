1ca2c6e6f6209ec46573c1bf6e6e4df0f94e2cca
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-01 11:33:43 -0800

    Time: 39 ms (52.32%), Space: 55 MB (23.39%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,3 +1,8 @@
+/*
+Hashmap + bucket sort
+time: O(n)
+space: O(n) 
+*/
 class Solution {
     public String frequencySort(String s) {
         HashMap<Character, Integer> freqMap = new HashMap<>();
@@ -7,19 +12,57 @@ class Solution {
             freqMap.put(ch, val+1);
         }
         
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(freqMap.get(b), freqMap.get(a)));
-        
-        for(char ch: freqMap.keySet()) {
-            maxHeap.add(ch);
+        List<List<Character>> buckets = new ArrayList<>();
+        for (int i=0; i<=s.length(); i++) {
+            buckets.add(new ArrayList<Character>());
         }
-                
+        
+        for (char ch: freqMap.keySet()) {
+            int freq = freqMap.get(ch);
+            buckets.get(freq).add(ch);
+        }
+         
         StringBuilder res = new StringBuilder();
-        while(!maxHeap.isEmpty()) {
-            char ch = maxHeap.poll();
-            int times = freqMap.get(ch);
-            while (times-- > 0)
-                res.append(ch);
+        for(int i=buckets.size()-1; i >= 0; i--) {
+            List<Character> chars = buckets.get(i);
+            for (char ch: chars) {
+                int times = freqMap.get(ch);
+                while (times-- > 0)
+                    res.append(ch);
+            }
         }
         return res.toString();
     }
 }
+
+
+// /*
+// Hashmap + priority queue
+// time: O(n + klogk) or O(n log n) ; k --> heap size; log k --> time for sorting/popping a heap (height)
+// space: O(n) 
+// */
+// class Solution {
+//     public String frequencySort(String s) {
+//         HashMap<Character, Integer> freqMap = new HashMap<>();
+        
+//         for (char ch: s.toCharArray()) {
+//             int val = freqMap.getOrDefault(ch, 0);
+//             freqMap.put(ch, val+1);
+//         }
+        
+//         PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(freqMap.get(b), freqMap.get(a)));
+        
+//         for(char ch: freqMap.keySet()) {
+//             maxHeap.add(ch);
+//         }
+                
+//         StringBuilder res = new StringBuilder();
+//         while(!maxHeap.isEmpty()) {
+//             char ch = maxHeap.poll();
+//             int times = freqMap.get(ch);
+//             while (times-- > 0)
+//                 res.append(ch);
+//         }
+//         return res.toString();
+//     }
+// }
