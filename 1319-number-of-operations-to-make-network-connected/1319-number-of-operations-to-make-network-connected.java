5fef2114504b77ccaf56a9e9f1ddee7391621123
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-04 01:04:07 -0800

    Time: 7 ms (89.45%), Space: 66.1 MB (46.02%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,45 @@
+class Solution {
+    private Integer[] parent;
+    private int[] rank;
+    
+    public int find(int node) {
+        int result = node;
+        while(this.parent[result] != result) {
+            result = this.parent[result];
+        }
+        return result;
+    }
+    
+    public void union(int node1, int node2) {
+        int par1 = find(node1), par2 = find(node2);
+        if(par1 != par2) {
+            if(rank[par2] > rank[par1]) {
+                this.parent[par1] = par2;
+                this.rank[par2] += this.rank[par1];
+            } else {
+                this.parent[par2] = par1;
+                this.rank[par1] += this.rank[par2];
+            }    
+        }    
+    }
+    
+    public int makeConnected(int n, int[][] connections) {
+        this.parent = new Integer[n];
+        this.rank = new int[n];
+        
+        for(int i=0; i<n; i++)
+            this.parent[i] = i;
+        
+        Arrays.fill(this.rank, 1);
+        
+        for(int[] nodes: connections) {
+            union(nodes[0], nodes[1]);
+        }
+        
+        Set<Integer> set = new HashSet();
+        for(int i=0;i<n;i++)
+            set.add(find(i));
+
+        return connections.length < n-1 ? -1 : set.size()-1;
+    }
+}
