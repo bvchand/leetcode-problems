688774137bb8c95e7832177f10e815bf51cacf53
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-08 20:46:12 -0800

    Time: 12 ms (92.35%), Space: 43.3 MB (74.71%) - LeetHub

--- 1226-the-dining-philosophers/1226-the-dining-philosophers.java
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -1,6 +1,7 @@
 class DiningPhilosophers {
 
-    public Lock[] forkLocks;
+    public ReentrantLock[] forkLocks;
+    private final Random rnd = new Random();  
 
     public DiningPhilosophers() {
         this.forkLocks = new ReentrantLock[5];
@@ -10,7 +11,7 @@ class DiningPhilosophers {
     }
 
     // call the run() method of any runnable to execute its code
-    public synchronized void wantsToEat(int phil,
+    public void wantsToEat(int phil,
                            Runnable pickLeftFork,
                            Runnable pickRightFork,
                            Runnable eat,
@@ -19,30 +20,56 @@ class DiningPhilosophers {
         
         // int nextPhil = (phil + 1) % 5;
         
-//         int leftFork = (phil + (phil % 2)) % 5;
-//         int rightFork = (phil + ((phil + 1) % 2)) % 5;
+        // int leftFork = (phil + (phil % 2)) % 5;
+        // int rightFork = (phil + ((phil + 1) % 2)) % 5;
         
-        int leftFork = phil;
-        int rightFork = (phil+1) % 5;
+        ReentrantLock rightFork = this.forkLocks[phil];
+        ReentrantLock leftFork = this.forkLocks[(phil + 1) % 5];
         
-        if (leftFork > rightFork) {
-            int temp = rightFork;
-            rightFork = leftFork;
-            leftFork = temp;
+//         int leftFork = phil;
+//         int rightFork = (phil+1) % 5;
+        
+//         if (leftFork > rightFork) {
+//             int temp = rightFork;
+//             rightFork = leftFork;
+//             leftFork = temp;
+//         }
+        
+        
+//         this.forkLocks[leftFork].lock();         // fork 1
+//         this.forkLocks[rightFork].lock();        // fork 2
+        
+//         pickLeftFork.run();
+//         pickRightFork.run();
+//         eat.run();
+//         putLeftFork.run();
+//         putRightFork.run();
+        
+//         this.forkLocks[leftFork].unlock();         // fork 1
+//         this.forkLocks[rightFork].unlock();        // fork 2
+        
+        boolean lf = false, rf = false;
+        
+        do {
+            rf = rightFork.tryLock(rnd.nextInt(100)+50, TimeUnit.MILLISECONDS);
+            lf = leftFork.tryLock(rnd.nextInt(100)+50, TimeUnit.MILLISECONDS); 
+            if(rf && !lf) {
+                rf = false;
+                rightFork.unlock();
+            }
+        } while(!(lf && rf));
+       
+        try{
+            pickRightFork.run();
+            pickLeftFork.run();
+eat.run();
+            
+            putLeftFork.run();
+            putRightFork.run();
+        } finally {
+            rightFork.unlock();
+            leftFork.unlock();
         }
-        
-        
-        this.forkLocks[leftFork].lock();         // fork 1
-        this.forkLocks[rightFork].lock();        // fork 2
-        
-        pickLeftFork.run();
-        pickRightFork.run();
-        eat.run();
-        putLeftFork.run();
-        putRightFork.run();
-        
-        this.forkLocks[leftFork].unlock();         // fork 1
-        this.forkLocks[rightFork].unlock();        // fork 2
     
     }
 }
