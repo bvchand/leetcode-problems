dbf0fa70a797d20b8be6e2482981eee92899c135
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-08 22:50:35 -0800

    Time: 10 ms (95.21%), Space: 41.5 MB (85.49%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,35 +1,61 @@
-class Foo {
+// class Foo {
     
-    private boolean firstDone = false;
-    private boolean secondDone = false;
+//     private boolean firstDone = false;
+//     private boolean secondDone = false;
 
+//     public Foo() {
+//     }
+
+//     public synchronized void first(Runnable printFirst) throws InterruptedException {
+        
+//         // printFirst.run() outputs "first". Do not change or remove this line.
+//         printFirst.run();
+//         this.firstDone = true;
+//         notifyAll();
+//     }
+
+//     public synchronized void second(Runnable printSecond) throws InterruptedException {
+        
+//         while(!this.firstDone) {
+//             wait();
+//         }
+//         // printSecond.run() outputs "second". Do not change or remove this line.
+//         printSecond.run();
+//         this.secondDone = true;
+//         notifyAll();
+//     }
+
+//     public synchronized void third(Runnable printThird) throws InterruptedException {
+//         while(!this.secondDone) {
+//             wait();
+//         }
+//         // printThird.run() outputs "third". Do not change or remove this line.
+//         printThird.run();
+//     }
+// }
+
+class Foo {
+    private Semaphore s2;
+    private Semaphore s3;
+    
     public Foo() {
+        s2 = new Semaphore(0);
+        s3 = new Semaphore(0);
     }
 
-    public synchronized void first(Runnable printFirst) throws InterruptedException {
-        
-        // printFirst.run() outputs "first". Do not change or remove this line.
+    public void first(Runnable printFirst) throws InterruptedException {
         printFirst.run();
-        this.firstDone = true;
-        notifyAll();
+        s2.release();
     }
 
-    public synchronized void second(Runnable printSecond) throws InterruptedException {
-        
-        while(!this.firstDone) {
-            wait();
-        }
-        // printSecond.run() outputs "second". Do not change or remove this line.
+    public void second(Runnable printSecond) throws InterruptedException {
+        s2.acquire();
         printSecond.run();
-        this.secondDone = true;
-        notifyAll();
+        s3.release();
     }
 
-    public synchronized void third(Runnable printThird) throws InterruptedException {
-        while(!this.secondDone) {
-            wait();
-        }
-        // printThird.run() outputs "third". Do not change or remove this line.
+    public void third(Runnable printThird) throws InterruptedException {
+        s3.acquire();
         printThird.run();
     }
 }
