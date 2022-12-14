8f334760f7ae8d8744af4f322c5d17453c909652
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-13 22:47:26 -0800

    Time: 0 ms (100.00%), Space: 40.1 MB (79.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,38 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] amountRobbed = new int[n+1];
         
-        amountRobbed[n] = 0;
-        amountRobbed[n-1] = nums[n-1];
+        if(n == 0)
+            return 0;
+        
+        int next2NextHouse = 0, nextHouse = nums[n-1];
         
         for(int i = n-2; i >= 0; i--) {
-            amountRobbed[i] = Math.max(nums[i] + amountRobbed[i+2], amountRobbed[i+1]);
+            int temp = Math.max(nums[i] + next2NextHouse, nextHouse);
+            next2NextHouse = nextHouse;
+            nextHouse = temp;
         }
         
-        return amountRobbed[0];
+        return nextHouse;
     }
 }
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] amountRobbed = new int[n+1];
+        
+//         amountRobbed[n] = 0;
+//         amountRobbed[n-1] = nums[n-1];
+        
+//         for(int i = n-2; i >= 0; i--) {
+//             amountRobbed[i] = Math.max(nums[i] + amountRobbed[i+2], amountRobbed[i+1]);
+//         }
+        
+//         return amountRobbed[0];
+//     }
+// }
+
+/*
+time & space: O(n)
+*/
