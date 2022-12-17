c86dc9b93246d89eb89728e52710d359295ace9c
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-17 15:22:31 -0800

    Time: 0 ms (100.00%), Space: 41.6 MB (97.66%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,20 +1,44 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-        for(int i=0; i<m; i++) {
-            if(target >= matrix[i][0] && target <= matrix[i][n-1]) {
-                int left = 0, right = n-1;
-                while(left <= right) {
-                    int mid = left + (right - left)/2;
-                    if(matrix[i][mid] == target)
-                        return true;
-                    else if(matrix[i][mid] > target)
-                        right = mid - 1;
-                    else
-                        left = mid + 1;
-                }
-            }  
+        
+        int left = 0, right = (m*n)-1;
+        
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            int i = mid / n, j = mid % n;
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
         return false;
     }
 }
+
+// O(m log n)
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int m = matrix.length, n = matrix[0].length;
+//         for(int i=0; i<m; i++) {
+//             if(target >= matrix[i][0] && target <= matrix[i][n-1]) {
+//                 int left = 0, right = n-1;
+//                 while(left <= right) {
+//                     int mid = left + (right - left)/2;
+//                     if(matrix[i][mid] == target)
+//                         return true;
+//                     else if(matrix[i][mid] > target)
+//                         right = mid - 1;
+//                     else
+//                         left = mid + 1;
+//                 }
+//             }  
+//         }
+//         return false;
+//     }
+// }
+
+// // O(m log n)
