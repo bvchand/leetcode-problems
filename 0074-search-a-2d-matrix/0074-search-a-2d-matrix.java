0046106534724e1c6b2c1a9f77e2a60ab05b2b3a
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-17 11:52:41 -0800

    Time: 0 ms (100.00%), Space: 42.2 MB (78.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,13 +3,13 @@ class Solution {
         int m = matrix.length, n = matrix[0].length;
         for(int i=0; i<m; i++) {
             if(target >= matrix[i][0] && target <= matrix[i][n-1]) {
-                int left = 0, right = n;
-                while(left < right) {
+                int left = 0, right = n-1;
+                while(left <= right) {
                     int mid = left + (right - left)/2;
                     if(matrix[i][mid] == target)
                         return true;
                     else if(matrix[i][mid] > target)
-                        right = mid;
+                        right = mid - 1;
                     else
                         left = mid + 1;
                 }
