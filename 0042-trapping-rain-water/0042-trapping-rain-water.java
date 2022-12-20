e346c7f4216df6a11b4fac2507969fb36d78373f
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-20 12:38:57 -0800

    Time: 1 ms (99.62%), Space: 49.2 MB (25.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,60 @@
 class Solution {
     public int trap(int[] height) {
-        int len = height.length;
-        int[] maxLeft = new int[len];
-        int[] maxRight = new int[len];
-        int leftMax = 0, rightMax = 0;
+        int N = height.length;
         
+        int left = 0, right = N-1;
+        int leftMax = 0, rightMax = 0;
         int ans = 0;
         
-        for(int i = 0; i < len; i++) {
-            leftMax = Math.max(height[i], leftMax);
-            maxLeft[i] = leftMax;
+        while(left <= right) {
+            if(height[left] <= height[right]) {
+                if(leftMax <= height[left]) {
+                    leftMax = height[left];
+                } else {
+                    ans += (leftMax - height[left]);
+                }
+                left++;
+            } else {
+                if(rightMax <= height[right]) {
+                    rightMax = height[right];
+                } else {
+                    ans += (rightMax - height[right]);
+                }
+                right--;
+            }
         }
-        
-        for(int i = len-1; i >= 0; i--) {
-            rightMax = Math.max(height[i], rightMax);
-            maxRight[i] = rightMax;
-        }
-        
-        for(int i = 1; i < len-1; i++) {
-            ans += Math.min(maxLeft[i], maxRight[i]) - height[i];
-        }
-        
+
         return ans;
     }
 }
+
+// Dynamic programming; time and space --> O(N)
+
+// class Solution {
+//     public int trap(int[] height) {
+//         int N = height.length;
+//         int[] maxLeft = new int[N];
+//         int[] maxRight = new int[N];
+//         int leftMax = 0, rightMax = 0;
+        
+//         int ans = 0;
+        
+//         for(int i = 0; i < N; i++) {
+//             leftMax = Math.max(height[i], leftMax);
+//             maxLeft[i] = leftMax;
+//         }
+        
+//         for(int i = N-1; i >= 0; i--) {
+//             rightMax = Math.max(height[i], rightMax);
+//             maxRight[i] = rightMax;
+//         }
+        
+//         for(int i = 1; i < N-1; i++) {
+//             ans += Math.min(maxLeft[i], maxRight[i]) - height[i];
+//         }
+        
+//         return ans;
+//     }
+// }
+
+// Dynamic programming; time and space --> O(N)
