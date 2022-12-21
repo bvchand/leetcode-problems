46a1f4bbb04505fc167e0fb0687afdda53edb3f2
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-20 16:46:59 -0800

    Time: 4 ms (77.98%), Space: 50.2 MB (32.42%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -15,14 +15,14 @@ class Solution {
             int cut1 = (left + right)/2;     // no. of elements in nums1
             int cut2 = (totalLen + 1)/2 - cut1;           // no. of elements in nums2
             
-            System.out.println(cut1 + " " + cut2);
+            // System.out.println(cut1 + " " + cut2);
             
             int left1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1-1];
             int left2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2-1];
             int right1 = cut1 == M ? Integer.MAX_VALUE : nums1[cut1];
             int right2 = cut2 == N ? Integer.MAX_VALUE : nums2[cut2];
             
-            System.out.println(left1 + " " + right1 + " " + left2 + " " + right2);
+            // System.out.println(left1 + " " + right1 + " " + left2 + " " + right2);
            
             if(left1 <= right2 && left2 <= right1)
                 return (totalLen % 2 == 0) ? ((double) Math.max(left1, left2) +  (double) Math.min(right1, right2)) / 2 : (double) Math.max(left1, left2);
