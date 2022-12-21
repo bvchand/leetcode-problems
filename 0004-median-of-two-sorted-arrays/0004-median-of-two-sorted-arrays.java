ac106068b6d48622cac6695d0ace000c27d7d5a6
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-20 16:47:27 -0800

    Time: 3 ms (83.86%), Space: 49.9 MB (43.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -13,19 +13,20 @@ class Solution {
         int left = 0, right = M;
         while(left <= right) {
             int cut1 = (left + right)/2;     // no. of elements in nums1
-            int cut2 = (totalLen + 1)/2 - cut1;           // no. of elements in nums2
+            int cut2 = (totalLen)/2 - cut1;           // no. of elements in nums2
             
             // System.out.println(cut1 + " " + cut2);
             
             int left1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1-1];
             int left2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2-1];
+            
             int right1 = cut1 == M ? Integer.MAX_VALUE : nums1[cut1];
             int right2 = cut2 == N ? Integer.MAX_VALUE : nums2[cut2];
             
             // System.out.println(left1 + " " + right1 + " " + left2 + " " + right2);
            
             if(left1 <= right2 && left2 <= right1)
-                return (totalLen % 2 == 0) ? ((double) Math.max(left1, left2) +  (double) Math.min(right1, right2)) / 2 : (double) Math.max(left1, left2);
+                return (totalLen % 2 == 0) ? ((double) Math.max(left1, left2) +  (double) Math.min(right1, right2)) / 2 : (double) Math.min(right1, right2);
             
             else if(left2 > right1)
                 left = cut1 + 1;
