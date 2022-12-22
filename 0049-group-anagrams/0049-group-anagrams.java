79409346df2f62604be83a74faeefba425f7864f
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-21 21:38:42 -0800

    Time: 39 ms (20.50%), Space: 46.4 MB (80.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,19 +2,18 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {        
         Map<String, List<String>> ans = new HashMap<>();
         int[] count = new int[26];
+        
         for(String str: strs) {
             Arrays.fill(count, 0);
             
             // get the count of every word in the input array
-            for(char c: str.toCharArray()) {
+            for(char c: str.toCharArray())
                 count[c-'a']++;
-            }
             
             // convert this count array into a string to add as a key into the map
             StringBuilder sb = new StringBuilder();
-            for(int cnt: count) {
+            for(int cnt: count)
                 sb.append(cnt + "#");
-            }
             
             // consolidate all the anagrams under one key
             ans.computeIfAbsent(sb.toString(), val -> new ArrayList<>()).add(str);
