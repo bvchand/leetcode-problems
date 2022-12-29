3b5e82821671fae50774457af55fa13ddc2913fd
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-29 09:56:23 -0800

    Time: 7 ms (25.13%), Space: 42.1 MB (85.89%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
@@ -1,25 +1,43 @@
+// class Solution {
+//     public int maxChunksToSorted(int[] arr) {
+//         int N = arr.length;
+        
+//         int[] maxFromLeft = new int[N];
+//         int[] minFromRight = new int[N];
+        
+//         maxFromLeft[0] = arr[0];
+//         for(int i=1; i<N; i++) {
+//             maxFromLeft[i] = Math.max(maxFromLeft[i-1], arr[i]);
+//         }
+        
+//         minFromRight[N-1] = arr[N-1];
+//         for(int i=N-2; i>=0; i--) {
+//             minFromRight[i] = Math.min(minFromRight[i+1], arr[i]);
+//         }
+        
+//         int ans = 1;
+//         for(int i=0; i<N-1; i++) {
+//             if(maxFromLeft[i] <= minFromRight[i+1])
+//                 ans++;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int N = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int maxTillNow = 0;
         
-        int[] maxFromLeft = new int[N];
-        int[] minFromRight = new int[N];
-        
-        maxFromLeft[0] = arr[0];
-        for(int i=1; i<N; i++) {
-            maxFromLeft[i] = Math.max(maxFromLeft[i-1], arr[i]);
+        for(int num: arr) {
+            maxTillNow = num;
+            
+            while(!stack.isEmpty() && stack.peek() > num)
+                maxTillNow = Math.max(maxTillNow, stack.pop());
+            
+            stack.add(maxTillNow);
         }
         
-        minFromRight[N-1] = arr[N-1];
-        for(int i=N-2; i>=0; i--) {
-            minFromRight[i] = Math.min(minFromRight[i+1], arr[i]);
-        }
-        
-        int ans = 1;
-        for(int i=0; i<N-1; i++) {
-            if(maxFromLeft[i] <= minFromRight[i+1])
-                ans++;
-        }
-        return ans;
+        return stack.size();
     }
 }
