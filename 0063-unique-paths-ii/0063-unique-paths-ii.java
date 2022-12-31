c063c49d17279d42e3c9480cdc6dc333775f81b5
bvchand <65008811+bvchand@users.noreply.github.com>
2022-12-30 19:36:10 -0800

    Time: 9 ms (5.63%), Space: 42.2 MB (46.99%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    int[][] grid;
+    int rows, cols;
+    final int OBSTACLE = 1, EMPTY = 0;
+    Map<String, Integer> seen = new HashMap<>();    // memoizing the path that could potentially lead to the goal
+    
+    
+    public int dfs(int r, int c) {
+        if(r < 0 || r >= rows || c < 0 || c >= cols) {
+            return 0;
+        }
+        
+        String key = Integer.toString(r) + "," + Integer.toString(c);
+        // System.out.println(key);
+        
+        if(seen.containsKey(key))
+            return seen.get(key);
+  
+        if(r == rows-1 && c == cols-1) {
+            if(grid[r][c] == OBSTACLE)  return 0;
+            return 1;
+        }
+        
+        if(grid[r][c] == OBSTACLE) {
+            seen.put(key, 0);
+            return 0;
+        }
+                
+        int down = dfs(r+1, c);
+        int right = dfs(r, c+1);
+        
+        seen.put(key, down + right);
+        return down + right;
+    }
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        this.grid = obstacleGrid;
+        this.rows = obstacleGrid.length;
+        this.cols = obstacleGrid[0].length;
+        
+        return dfs(0, 0);
+    }
+}
