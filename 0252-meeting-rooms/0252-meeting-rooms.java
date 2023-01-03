b125ab6e61fcf71e596a00cbd7b2e4924cf9c961
bvchand <65008811+bvchand@users.noreply.github.com>
2023-01-02 22:07:51 -0800

    Time: 10 ms (54.01%), Space: 45.4 MB (13.36%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -3,9 +3,9 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
                 
         for(int i=0; i<intervals.length-1; i++) {
-            int[] curr = intervals[i];
-            int[] next = intervals[i+1];
-            if(next[0] < curr[1])
+            // int[] curr = intervals[i];
+            // int[] next = intervals[i+1];
+            if(intervals[i+1][0] < intervals[i][1])
                 return false;
         }
         
