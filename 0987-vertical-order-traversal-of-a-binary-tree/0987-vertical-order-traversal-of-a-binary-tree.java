405115fba92cc120d8cc67096d4c914fb32fdb59
bvchand <65008811+bvchand@users.noreply.github.com>
2023-01-04 23:51:14 -0800

    Time: 4 ms (76.39%), Space: 42.8 MB (55.30%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        Map<Integer, List<Pair<Integer, Integer>>> colMap = new TreeMap<>();
+        Queue<Pair<TreeNode, Pair<Integer, Integer>>> queue = new LinkedList<>();
+        queue.offer(new Pair(root, new Pair(0, 0)));
+        
+        while(!queue.isEmpty()) {
+            Pair<TreeNode, Pair<Integer, Integer>> node = queue.poll();
+            TreeNode curr = node.getKey();
+            int level = node.getValue().getKey();   // row
+            int index = node.getValue().getValue(); // col
+            
+            if(curr != null) {
+                colMap.computeIfAbsent(index, val -> new ArrayList<>()).add(new Pair(level, curr.val));
+                queue.offer(new Pair(curr.left, new Pair(level+1, index-1)));
+                queue.offer(new Pair(curr.right, new Pair(level+1, index+1)));
+            }
+        }
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        for(List<Pair<Integer, Integer>> colList: colMap.values()) {
+            // System.out.println(colList);
+            Collections.sort(colList, new Comparator<Pair<Integer, Integer>>() {
+                @Override
+                public int compare(Pair<Integer, Integer> a, Pair<Integer, Integer> b) {
+                    if(a.getKey() == b.getKey())
+                        return a.getValue() - b.getValue();
+                    else
+                        return a.getKey() - b.getKey();
+                }
+            });
+            
+            List<Integer> temp = new ArrayList<>();
+            for(Pair<Integer, Integer> node: colList)
+                temp.add(node.getValue());
+            res.add(temp);
+        }
+        return res;
+    }
+}
