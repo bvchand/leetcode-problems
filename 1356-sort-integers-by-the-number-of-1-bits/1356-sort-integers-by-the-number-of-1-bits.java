bc37a5d2063c26dc076e31eed3e1f7387c3f689e
bvchand <65008811+bvchand@users.noreply.github.com>
2023-01-11 22:28:17 -0800

    Time: 8 ms (73.64%), Space: 42.7 MB (70.03%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,17 +1,44 @@
-// bit manipulation
+// priority queue
 class Solution {
     public int[] sortByBits(int[] arr) {
         int n = arr.length;
-        // int[] bits = new int[n];
-        for(int i = 0; i<n; i++)
-            arr[i] += Integer.bitCount(arr[i]) * 10001;
+        int[] bits = new int[10001];
+        for(int num: arr)
+            bits[num] = Integer.bitCount(num);
         
-        System.out.println(Arrays.toString(arr));
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            if(bits[a] == bits[b])
+                return a - b;
+            return bits[a] - bits[b];
+        });
+        
         for(int i=0; i<n; i++)
-            arr[i] %= 10001;
+            pq.add(arr[i]);
         
-        return arr;
+        int index = 0;
+        int[] res = new int[n];
+        while(!pq.isEmpty()) {
+            res[index++] = pq.poll();
+        }
+        
+        return res;
     }
 }
+
+// bit manipulation
+// class Solution {
+//     public int[] sortByBits(int[] arr) {
+//         int n = arr.length;
+//         // int[] bits = new int[n];
+//         for(int i = 0; i<n; i++)
+//             arr[i] += Integer.bitCount(arr[i]) * 10001;
+        
+//         System.out.println(Arrays.toString(arr));
+//         Arrays.sort(arr);
+//         System.out.println(Arrays.toString(arr));
+//         for(int i=0; i<n; i++)
+//             arr[i] %= 10001;
+        
+//         return arr;
+//     }
+// }
