472f1527339e1322d4958c99f3431530f1641f4d
bvchand <65008811+bvchand@users.noreply.github.com>
2023-01-12 16:33:34 -0800

    Time: 28 ms (91.86%), Space: 57.3 MB (77.95%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -4,18 +4,20 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         
         // minheap
-        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a, b) ->((a.get(0) + a.get(1)) - (b.get(0) + b.get(1))));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->((a[0] + a[1]) - (b[0] + b[1])));
         
         for(int i=0; i<Math.min(m, k); i++) {
-            pq.offer(Arrays.asList(nums1[i], nums2[0], 0));
+            pq.offer(new int[]{nums1[i], nums2[0], 0});
         }
         
         while(k != 0 && !pq.isEmpty()) {
-            List<Integer> curr = pq.poll();
-            int num1 = curr.get(0), num2 = curr.get(1), idx = curr.get(2);
+            int[] curr = pq.poll();
+            int num1 = curr[0], num2 = curr[1], idx = curr[2];
+            
             res.add(Arrays.asList(num1, num2));
+            
             if(idx < n-1) {
-                pq.offer(Arrays.asList(num1, nums2[idx+1], idx+1));
+                pq.offer(new int[]{num1, nums2[idx+1], idx+1});
             }
             k--;
         }
