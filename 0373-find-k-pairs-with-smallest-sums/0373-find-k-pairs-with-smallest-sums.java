9b18e1d7b0c9066428dfc1e86276de22e4bf2282
bvchand <65008811+bvchand@users.noreply.github.com>
2023-01-12 16:38:00 -0800

    Time: 34 ms (79.13%), Space: 57.3 MB (79.13%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,3 +1,6 @@
+// priority queue
+// time: O(klogk) space:O(k)
+
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         int m = nums1.length, n = nums2.length;
@@ -10,14 +13,15 @@ class Solution {
             pq.offer(new int[]{nums1[i], nums2[0], 0});
         }
         
+        // every iteration --> poll the least pair till now, add that to the res and add the next pair by incrementing the nums2 index
         while(k != 0 && !pq.isEmpty()) {
             int[] curr = pq.poll();
-            int num1 = curr[0], num2 = curr[1], idx = curr[2];
+            int num1 = curr[0], num2 = curr[1], nums2Idx = curr[2];
             
             res.add(Arrays.asList(num1, num2));
             
-            if(idx < n-1) {
-                pq.offer(new int[]{num1, nums2[idx+1], idx+1});
+            if(nums2Idx < n-1) {
+                pq.offer(new int[]{num1, nums2[nums2Idx+1], nums2Idx+1});
             }
             k--;
         }
