c728dc7097689cb8827c0d66f4e7642ebacad76c
bvchand <65008811+bvchand@users.noreply.github.com>
2023-01-18 22:46:16 -0800

    Time: 12 ms (62.88%), Space: 42.6 MB (91.71%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -1,19 +1,39 @@
-class Solution {
-    public int countBinarySubstrings(String s) {
-        int preCnt = 0, curCnt = 1,  ans = 0, n = s.length();
+// class Solution {
+//     public int countBinarySubstrings(String s) {
+//         int preCnt = 0, curCnt = 1,  ans = 0, n = s.length();
         
-        for(int i = 1; i < n; i++) {
-            if(s.charAt(i) == s.charAt(i-1))
-                curCnt++;
-            else {
-                ans += Math.min(preCnt, curCnt);
-                preCnt = curCnt;
-                curCnt = 1;
-            }
-        }
-        return ans + Math.min(preCnt, curCnt);
-    }
-}
+//         for(int i = 1; i < n; i++) {
+//             if(s.charAt(i) == s.charAt(i-1))
+//                 curCnt++;
+//             else {
+//                 ans += Math.min(preCnt, curCnt);
+//                 preCnt = curCnt;
+//                 curCnt = 1;
+//             }
+//         }
+//         return ans + Math.min(preCnt, curCnt);
+//     }
+// }
 
 // maintain 2 counters (preCnt and curCnt) to count the number of matches found so far
 // 
+
+
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int preCnt = 0, ans = 0, n = s.length();
+        
+        for(int i = 0; i < n; i++) {
+            int curCnt = 1;
+            while(i < n-1 && s.charAt(i) == s.charAt(i+1)) {
+                curCnt++;
+                i++;
+            }
+            if(preCnt > 0) {
+                ans += Math.min(preCnt, curCnt);
+            }
+            preCnt = curCnt;
+        }
+        return ans;
+    }
+}
